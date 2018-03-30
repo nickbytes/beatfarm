22a3b10d3d334cdb067747d158744851cd1513c8
Nick Beattie <nick@nickbytes.com>
2018-03-30 13:58:44 -0400

    why am i using typescript?

---
+++ src/components/Row/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const Row = () => (
+  <div>
+    <h1>hi</h1>
+  </div>
+);
+
+export default Row;
---
+++ src/components/Step/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const StepSpan = styled.span`
+  width: 16px;
+  height: 16px;
+  background-color: rgba(62, 111, 173, 1);
+`;
+
+const Step = () => <StepSpan />;
+
+export default Step;
