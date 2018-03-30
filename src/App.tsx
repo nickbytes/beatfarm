1b07cdb6e90a678f5f8ca487351894c597674464
Nick Beattie <nick@nickbytes.com>
2018-03-30 15:06:46 -0400

    yea nick do the css and not the real project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
 import * as React from "react";
+import styled from "styled-components";
 import Row from "./components/Row";
+
+const AppContainer = styled.div`
+  margin: 0;
+  padding: 50px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+// You're all probably wondering why I don't order my css...
+// That's a good question.
+const Title = styled.h1`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  font-weight: normal;
+  margin: 0;
+  font-size: 17px;
+  line-height: 22px;
+`;
 
 class App extends React.Component {
   state = {
@@ -16,10 +43,10 @@ class App extends React.Component {
   };
   render() {
     return (
-      <div>
-        <h1>beatfarm</h1>
+      <AppContainer>
+        <Title>beatfarm</Title>
         {this.state.rows.map(rowItem => <Row key={rowItem.id} />)}
-      </div>
+      </AppContainer>
     );
   }
 }
